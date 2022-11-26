7c58f59d06ab58748c365a2f006f5dcfe6f4afc0
Divyatestgithub <118978781+Divyatestgithub@users.noreply.github.com>
2022-11-26 21:55:41 +0530

    Update networking.tf

--- networking.tf
+++ networking.tf
@@ -29,9 +29,9 @@ resource "azurerm_network_security_group" "appnsg" {
     priority                   = 300
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "Tcp"
+    protocol                   = "ssh"
     source_port_range          = "*"
-    destination_port_range     = "3389"
+    destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
